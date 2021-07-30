5be6957f12bebc24124ecff08df8f5d682dd3c87
malvdev <sergey.malkov.dev@gmail.com>
2021-07-30 22:40:13 +0500

    feat(app): include ngrx store

--- apps/trello/src/app/app.module.ts
+++ apps/trello/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { APP_CONFIG } from '@trello/libs/config';
+import { AuthDomainModule } from '@trello/libs/auth/domain';
 
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +18,20 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     RouterModule,
+    AuthDomainModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
     AppRoutingModule, // must be imported as the last module as it contains the fallback route
   ],
   providers: [{ provide: APP_CONFIG, useValue: environment }],
