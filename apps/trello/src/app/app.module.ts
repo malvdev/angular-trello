1535de6c5aa7520f44fee137f7f6578dc1e06bec
malvdev <sergey.malkov.dev@gmail.com>
2021-07-30 13:35:54 +0500

    feat(app): add fallback route

--- apps/trello/src/app/app.module.ts
+++ apps/trello/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { APP_CONFIG } from '@trello/libs/config';
 
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule, // must be imported as the last module as it contains the fallback route
+  ],
   providers: [{ provide: APP_CONFIG, useValue: environment }],
   bootstrap: [AppComponent],
 })
