8c5cfd7441ca3d244c9cb0f054cb3dc5d9a7fe5c
malvdev <sergey.malkov.dev@gmail.com>
2021-07-30 22:27:28 +0500

    feat(auth-domain): include providers interceptors

--- libs/auth/domain/src/lib/auth-domain.module.ts
+++ libs/auth/domain/src/lib/auth-domain.module.ts
@@ -1,15 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import {
   AuthGuard,
   AuthService,
   CredentialsService,
   StoragesModule,
+  AuthInterceptor,
+  ErrorHandlerInterceptor,
 } from './infrastructure';
 
 @NgModule({
   imports: [CommonModule, StoragesModule],
-  providers: [AuthService, CredentialsService, AuthGuard],
+  providers: [
+    AuthService,
+    CredentialsService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorHandlerInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
 })
-export class AuthDomainModule {}
+export class AuthDomainModule {
+  constructor(@Optional() @SkipSelf() parentModule: AuthDomainModule) {
+    if (parentModule) {
+      throw new Error(
+        `${parentModule} has already been loaded. It should only be imported in your application's main module.`
+      );
+    }
+  }
+}
