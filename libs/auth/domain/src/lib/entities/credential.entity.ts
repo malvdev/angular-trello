dab1d5b4f25e06b52ddc7d047d4ba45b8b3da96c
malvdev <sergey.malkov.dev@gmail.com>
2021-07-30 21:50:02 +0500

    refactor(auth-domain): add credential entity

---
+++ libs/auth/domain/src/lib/entities/credential.entity.ts
@@ -0,0 +1,6 @@
+export interface CredentialEntity {
+  id?: string;
+  email: string;
+  accessToken: string;
+  refreshToken: string;
+}
--- libs/auth/domain/src/lib/entities/index.ts
+++ libs/auth/domain/src/lib/entities/index.ts
@@ -1,2 +1,5 @@
-export * from './login-user.entity';
+export * from './credential.entity';
+export * from './forgot.entity';
 export * from './register-user.entity';
+export * from './login-user.entity';
+export * from './credential.entity';
