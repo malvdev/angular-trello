496daafa1382261092ef831c391186fb401aef76
malvdev <sergey.malkov.dev@gmail.com>
2021-07-30 16:00:09 +0500

    feat(welcome): add home content component

--- libs/welcome/feature-home/src/lib/welcome-feature-home.module.ts
+++ libs/welcome/feature-home/src/lib/welcome-feature-home.module.ts
@@ -4,8 +4,11 @@ import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { HomeContentComponent } from './home-content';
 
 @NgModule({
+  declarations: [HomeComponent, HomeContentComponent],
   imports: [
     CommonModule,
     RouterModule,
