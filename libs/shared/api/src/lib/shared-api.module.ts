a8f0ff62bc4e9ae3b6e0028a49fd4a5bb754f654
malvdev <sergey.malkov.dev@gmail.com>
2021-07-30 20:08:40 +0500

    refactor(shared-ui): move api service in infrastructure

--- libs/shared/api/src/lib/shared-api.module.ts
+++ libs/shared/api/src/lib/shared-api.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ApiService } from './infrastructure';
+
 @NgModule({
   imports: [CommonModule],
+  providers: [ApiService],
 })
 export class SharedApiModule {}
