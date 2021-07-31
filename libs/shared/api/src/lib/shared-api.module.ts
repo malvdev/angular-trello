2f11c3ec5c92aae2106146ea4af45f9794876800
malvdev <sergey.malkov.dev@gmail.com>
2021-07-31 13:19:33 +0500

    fix(shared-api): import http client module

--- libs/shared/api/src/lib/shared-api.module.ts
+++ libs/shared/api/src/lib/shared-api.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ApiService } from './infrastructure';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   providers: [ApiService],
 })
 export class SharedApiModule {}
