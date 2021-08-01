ba90320a5bf87517d183cdc65f3aff88d34de497
malvdev <sergey.malkov.dev@gmail.com>
2021-08-01 22:49:54 +0500

    feat(profile): add profile domain

---
+++ libs/profile/domain/README.md
@@ -0,0 +1,5 @@
+# profile-domain
+
+## Running unit tests
+
+Run `nx test profile-domain` to execute the unit tests.
---
+++ libs/profile/domain/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'profile-domain',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/profile/domain',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
