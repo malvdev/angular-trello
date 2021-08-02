0b3f63366be0b9fd5bb66030f461036a245ef865
malvdev <sergey.malkov.dev@gmail.com>
2021-08-02 13:31:34 +0500

    feat(shared-ui): import header in main template

--- libs/shared/ui/src/lib/main-template/main-template.module.ts
+++ libs/shared/ui/src/lib/main-template/main-template.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeaderModule } from '../header';
 import { MainTemplateComponent } from './main-template.component';
 
 @NgModule({
   declarations: [MainTemplateComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderModule],
   exports: [MainTemplateComponent],
 })
 export class MainTemplateModule {}
