aa3ce58b7bf2157fd38c7d81a12d27da179ec4fe
malvdev <sergey.malkov.dev@gmail.com>
2021-08-03 12:15:12 +0500

    refactor(board-domain): change types for card entity

--- libs/board/domain/src/lib/entities/card.entity.ts
+++ libs/board/domain/src/lib/entities/card.entity.ts
@@ -1,10 +1,12 @@
+import { BadgeEntity } from './badge.entity';
+import { LabelEntity } from './label.entity';
 import { MemberEntity } from './member.entity';
 
 export interface CardEntity {
   id: string;
   title: string;
   description?: string;
-  labels?: { [key: string]: string }[];
-  badges?: { [key: string]: string }[];
+  labels?: LabelEntity[];
+  badges?: BadgeEntity[];
   members?: MemberEntity[];
 }
--- libs/board/domain/src/lib/entities/index.ts
+++ libs/board/domain/src/lib/entities/index.ts
@@ -1,3 +1,6 @@
+export * from './badge.entity';
 export * from './board.entity';
 export * from './card.entity';
 export * from './list.entity';
+export * from './label.entity';
+export * from './member.entity';
